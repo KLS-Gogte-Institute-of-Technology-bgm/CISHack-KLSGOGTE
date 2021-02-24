38f14ef237b06b8ecc20aba72f1b36520e2fadc3
shreyaskapale <shreyas.kapale@gmail.com>
2021-02-24 09:10:56 +0530

    del

--- clinic-management-main/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "***Checking for latest version of image***"
-docker pull shreyasssk/clinic-server
-
-echo "***deploying...***"
-docker-compose up
--- clinic-management-main/docker-deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker build -t shreyasssk/clinic-server:latest -t shreyasssk/clinic-server:$SHA .
-
-docker push shreyasssk/clinic-server:latest
-docker push shreyasssk/clinc-server:$SHA
-
-echo "DONE! ;)"
